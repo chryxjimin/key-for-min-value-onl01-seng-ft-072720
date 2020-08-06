325c987991a2c71243357873cbbd01ab4aeccaae
Christie Yoon <cleex1999@gmail.com>
2020-08-06 00:15:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
+require 'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
 
-end
+
+
+def key_for_min_value(name_hash)
+    i = 0
+  name_hash.collect do |key, value|
+    puts name_hash[key]
+    i += 1
+    end
+    nil
+  end
+
+  #binding.pry
